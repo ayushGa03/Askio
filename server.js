8f6a7962aac9aba44ce66a9de56cadfb597c9a85
ayushGa03 <ayushgaurav.dev@gmail.com>
2026-06-18 23:28:42 +0530

    fixed server file

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import app from "./src/app.js"
+import db from "./config/Databse.js"
+import cache from "./config/cache.js"
+import cors from "cors"
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import http from "http";
+import {connectToSocket} from "./src/sokets/soketsio.js";
+
+dotenv.config();
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET","POST","PUT","DELETE"],
+}))
+const httpServer = http.createServer(app);
+db();
+
+const port = 3030;
+connectToSocket(httpServer);// called the socket connection function and passed the http server to it
+httpServer.listen(port,()=>{
+    console.log(`Server is listening to the port${port}`)
+
+});
